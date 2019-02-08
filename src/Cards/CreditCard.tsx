e9139c30b06abbeb29e1e9faa13280e2028a08f1
efc <efc@clst.org>
2019-02-08 15:31:25 -0600

    more credits

--- src/Cards/CreditCard.tsx
+++ src/Cards/CreditCard.tsx
@@ -38,6 +38,10 @@ export class CreditCard extends React.Component<Props, State> {
 			>
 				<p>We love the walking subcaucus process and it makes us a bit sad that the squirrelly math required to calculate who gets how many delegate discourages meetings and caucuses from using the process. We hope this calculator makes it easier for you to get to know your neighbors as you work together to change the world!</p>
 				<p>Please check us out at <a href="https://tenseg.net">tenseg.net</a> if you need help building a website or making appropriate use of technology.</p>
+				<h3>Licenses</h3>
+				<p>This calculator's code is available under the MIT License at Bitbucket.</p>
+				<p>It makes use of <a href="https://reactjs.org/">ReactJS</a>, <a href="https://www.typescriptlang.org/">TypeScript</a>, <a href="https://www.primefaces.org/primereact">PrimeReact</a>, <a href="https://www.primefaces.org/primeng/#/icons">PrimeIcons</a>, <a href="https://github.com/mojotech/json-type-validation">JSON Type Validation</a>, <a href="https://github.com/ClickSimply/typescript-map">typescript-map</a>, and many other open source projects.
+				</p>
 				<div className="fineprint">
 					<>v {packageJson.version}</>
 					{_u.isApp()
